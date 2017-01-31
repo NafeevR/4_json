692f3a19e3582f5caafe0f79b99f1f39162b47e5
NafeevR <nafeev.r@yandex.ru>
2017-01-31 13:54:58 +0300

    ...

--- pprint_json.py
+++ pprint_json.py
@@ -13,6 +13,6 @@ def pretty_print_json(data):
 
 
 if __name__ == '__main__':
-    path_to_the_file = input('Input path to the file = ')
-    json_info = load_data(path_to_the_file)
-    pretty_print_json(json_info)
+    path_to_file = input('Input path to the file = ')
+    json_info = load_data(path_to_file)
+    pretty_print_for_json(json_info)
